5b7aa6dfb11265b235c423d99ef84534f01329f8
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-11 18:41:08 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
-﻿
-const fs = require('fs')
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require('fs')
+
 
 const welcome = JSON.parse(fs.readFileSync('./welcomer.json' , 'utf8'));
  
